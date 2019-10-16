f15c60c649ad398e85cf79b08d07a072175a213e
wanglei <920199044@qq.com>
2019-10-16 17:47:20 +0800

    前台首页画面修改

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -15,15 +15,15 @@ export const Home = (props: IHomeProp) => {
 
   return (
     <Row>
-      <Col md="9">
+      <Col md="9" className="ocrTips">
         <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
+          <Translate contentKey="home.title">Welcome, OCR users!</Translate>
         </h2>
         <p className="lead">
           <Translate contentKey="home.subtitle">This is your homepage</Translate>
         </p>
         {account && account.login ? (
-          <div>
+          <div className="loginUser">
             <Alert color="success">
               <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
                 You are logged in as user {account.login}.
@@ -53,47 +53,53 @@ export const Home = (props: IHomeProp) => {
           </div>
         )}
         <p>
-          <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
+          <Translate contentKey="home.question">If you have any question on OCR:</Translate>
         </p>
 
         <ul>
           <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
+            <a href="https://ai.baidu.com//" target="_blank" rel="noopener noreferrer">
+              <Translate contentKey="home.link.homepage">Baidu AI developer platform homepage</Translate>
             </a>
           </li>
+          <br/>
           <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
+            <a href="https://ai.baidu.com/tech/ocr" target="_blank" rel="noopener noreferrer">
+              <Translate contentKey="home.link.stackoverflow">Learn about OCR on Baidu</Translate>
             </a>
           </li>
+          <br/>
           <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
+            <a href="https://ai.baidu.com/tech/ocr/iocr" target="_blank" rel="noopener noreferrer">
+              <Translate contentKey="home.link.bugtracker">Create custom OCR templates on Baidu</Translate>
             </a>
           </li>
+          <br/>
           <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
+            <a href="https://ai.baidu.com/docs#/iOCR-Guide/top" target="_blank" rel="noopener noreferrer">
+              <Translate contentKey="home.link.chat">Custom template text recognition on IOCR</Translate>
             </a>
           </li>
+          <br/>
           <li>
-            <a href="https://twitter.com/java_hipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.follow">follow @java_hipster on Twitter</Translate>
+            <a href="https://ai.baidu.com/forum/topic/list/164" target="_blank" rel="noopener noreferrer">
+              <Translate contentKey="home.link.follow">OCR public chat room</Translate>
             </a>
           </li>
         </ul>
 
         <p>
-          <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
+          <Translate contentKey="home.like">If you have any comments or Suggestions, please let me know</Translate>{' '}
+          <a href="http://192.168.20.90/wanglei/jhipster_ivision" target="_blank" rel="noopener noreferrer">
             Github
           </a>
           !
         </p>
       </Col>
       <Col md="3" className="pad">
-        <span className="hipster rounded" />
+      <a href="http://www.linxincn.com" target="-blank" title="文字识别">
+    <img src="../../../content/images/jhipster_ivision_ocr_2.JPG" className="ocrImg" />
+      </a>
       </Col>
     </Row>
   );
