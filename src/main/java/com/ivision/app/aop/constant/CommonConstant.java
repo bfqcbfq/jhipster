3deb23c48a9c3f40857a8d0a6eb9599e6b0c3981
wanglei <920199044@qq.com>
2019-11-15 10:43:51 +0800

    update by wanglei

---
+++ src/main/java/com/ivision/app/aop/constant/CommonConstant.java
@@ -0,0 +1,55 @@
+package com.ivision.app.aop.constant;
+
+/**
+ * @author wanglei 
+ * 
+ * <p>
+ * 共同常量定义
+ * </p>
+ */
+public class CommonConstant {
+
+	/**
+	 * 自定义识别模板
+	 * 
+	 * 北京银丰科技有限公司出货单
+	 */
+	public static final String OCR_IOCR_YINGFENG_TITLE = "北京银丰科技有限公司出货单";
+	/**
+	 * 东莞明歆制衣有限公司出库单
+	 */
+	public static final String OCR_IOCR_MINGXING_TITLE = "东莞明歆制衣有限公司出库单";
+	/**
+	 * 易达软件出货单
+	 */
+	public static final String OCR_IOCR_YIDA_TITLE = "易达软件出货单";
+	/**
+	 * 
+	 * 文件·类型
+	 * 北京银丰科技有限公司出货单:type=1
+	 */
+	public static final String OCR_IOCR_YINGFENG_TYPE = "1";
+	/**
+	 * 
+	 * 文件·类型
+	 * 东莞明歆制衣有限公司出库单:type=2
+	 */
+	public static final String OCR_IOCR_MINGXING_TYPE = "2";
+	/**
+	 * 
+	 * 文件·类型
+	 * 易达软件出货单:type=3
+	 */
+	public static final String OCR_IOCR_YIDA_TYPE = "3";
+	
+	/**
+	 * 百度文字识别错误代码
+	 * 返回成功代码0
+	 */
+	public static final String OCR_IOCR_ERRORCODE = "0";
+	/**
+	 * Ivision/NRI调查问卷标题
+	 */
+	public static final String OCR_GENERAL_SURVEY_TITLE = "「中国サプライチェーンを取り巻くデジタライゼーション」からのアンケート";
+
+}
